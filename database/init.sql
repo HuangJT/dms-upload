e071f7134519400dada89c2e22112ebb30f8d2f0
Gavin1995 <gavin199502@gmail.com>
2019-01-24 15:00:14 +0800

    feat(init.sql): add init.sql

---
+++ database/init.sql
@@ -0,0 +1,3 @@
+USE winwinfe_dms_upload;
+
+INSERT INTO `du_user` (`id`,`employee_id`,`nickname`,`real_name`,`phone_number`,`username`,`password`,`type`,`avatar`,`wechat`,`email`,`gender`,`soft_delete`,`create_time`,`update_time`,`create_guid`) VALUES (1,0,NULL,NULL,NULL,'root','fc27254f32e2b3613c52df57d6ac58ef',2,NULL,NULL,NULL,0,0,'2019-01-23 18:28:53','2019-01-23 18:28:53','02ba3077-5bcc-429a-99ec-e213180382a8');
